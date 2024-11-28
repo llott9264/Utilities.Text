b35439444245f7cb82833aa63d25f5a5c2e6292f
Lee Lott <llott88@gmail.com>
2024-11-28 07:13:15 -0600

    #10 - Created Right() method and unit tests.

--- src/Utilities.Text/Utilities.cs
+++ src/Utilities.Text/Utilities.cs
@@ -8,5 +8,12 @@
 				? value
 				: value.Substring(0, Math.Min(length, value.Length));
 		}
+
+		public static string Right(this string value, int length)
+		{
+			return string.IsNullOrEmpty(value)
+				? value
+				: value.Substring(Math.Max(0, value.Length - length));
+		}
 	}
 }
