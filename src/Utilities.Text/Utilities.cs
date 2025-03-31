f1bb49af6e13a5595b75ca87b652138ad5c0b9df
Lee Lott <llott88@gmail.com>
2025-03-30 19:49:24 -0500

    #19 - Added editorconfig file.

--- src/Utilities.Text/Utilities.cs
+++ src/Utilities.Text/Utilities.cs
@@ -1,61 +1,63 @@
 ﻿using System.Globalization;
 using System.Text;
 
-namespace Utilities.Text
+namespace Utilities.Text;
+
+public static class Utilities
 {
-	public static class Utilities
-	{ 
-		public static string Left(this string value, int length)
+	public static string Left(this string value, int length)
+	{
+		return string.IsNullOrEmpty(value)
+			? value
+			: value.Substring(0, Math.Min(length, value.Length));
+	}
+
+	public static string Right(this string value, int length)
+	{
+		return string.IsNullOrEmpty(value)
+			? value
+			: value.Substring(Math.Max(0, value.Length - length));
+	}
+
+	public static string RemoveCharacters(this string value, List<string> charactersToRemove)
+	{
+		Dictionary<string, string> charactersToReplace = new();
+		charactersToRemove.ForEach(s => charactersToReplace.Add(s, ""));
+		return ReplaceCharacters(value, charactersToReplace);
+	}
+
+	public static string RemoveCrLf(this string value)
+	{
+		return RemoveCharacters(value, ["\n", "\r"]);
+	}
+
+	public static string ReplaceCharacters(this string value, Dictionary<string, string> charactersToReplace)
+	{
+		return string.IsNullOrEmpty(value)
+			? string.Empty
+			: charactersToReplace.Aggregate(value,
+				(current, keyValuePair) => current.Replace(keyValuePair.Key, keyValuePair.Value));
+	}
+
+	public static string ReplaceDiacritics(this string value)
+	{
+		if (string.IsNullOrEmpty(value))
 		{
-			return string.IsNullOrEmpty(value)
-				? value
-				: value.Substring(0, Math.Min(length, value.Length));
+			return value;
 		}
 
-		public static string Right(this string value, int length)
-		{
-			return string.IsNullOrEmpty(value)
-				? value
-				: value.Substring(Math.Max(0, value.Length - length));
-		}
-		
-		public static string RemoveCharacters(this string value, List<string> charactersToRemove)
-		{
-			Dictionary<string, string> charactersToReplace = new();
-			charactersToRemove.ForEach(s => charactersToReplace.Add(s, ""));
-			return ReplaceCharacters(value, charactersToReplace);
-		}
+		string normalizedString = value.Normalize(NormalizationForm.FormD);
+		StringBuilder stringBuilder = new();
 
-		public static string RemoveCrLf(this string value)
+		foreach (char c in normalizedString)
 		{
-			return RemoveCharacters(value, ["\n", "\r"]);
-		}
-
-		public static string ReplaceCharacters(this string value, Dictionary<string, string> charactersToReplace)
-		{
-			return string.IsNullOrEmpty(value)
-				? string.Empty
-				: charactersToReplace.Aggregate(value, (current, keyValuePair) => current.Replace(keyValuePair.Key, keyValuePair.Value));
-		}
-
-		public static string ReplaceDiacritics(this string value)
-		{
-			if (string.IsNullOrEmpty(value))
-				return value;
-
-			string normalizedString = value.Normalize(NormalizationForm.FormD);
-			StringBuilder stringBuilder = new();
-
-			foreach (char c in normalizedString)
+			UnicodeCategory unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+			if (unicodeCategory != UnicodeCategory.NonSpacingMark)
 			{
-				UnicodeCategory unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-				if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-				{
-					stringBuilder.Append(c);
-				}
+				stringBuilder.Append(c);
 			}
-
-			return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
 		}
+
+		return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
 	}
 }
